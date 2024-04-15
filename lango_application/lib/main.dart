10166eb2560984415b2e7a56adba7c8ffcb0419b
Chayapol Mahatthanachai <best37@hotmail.com>
2024-04-15 22:47:04 +0700

    [ADD] Sign In / Sign Up page

--- lango_application/lib/main.dart
+++ lango_application/lib/main.dart
@@ -5,16 +5,55 @@ import 'theme/custom_theme.dart';
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: CustomTheme.customTheme,
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Lango',
+      home: Scaffold(
+        backgroundColor: Color(0xFFFEF9EF),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Image(image: AssetImage('../assets/logos/lango_logo.png'),
+              width: 450,
+              height: 450,),
+              const SizedBox(height: 50,),
+              ElevatedButton(
+                onPressed: () {},
+                child: SizedBox(
+                  width: 255,
+                  child: Text('GET STARTED', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'inter', fontSize: 18, fontWeight: FontWeight.bold),),
+                ),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white, backgroundColor: Color(0xFFFEE440),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                  onPressed: () {}, child: Text('ALREADY HAVE AN ACCOUNT', style: TextStyle(fontFamily: 'inter', fontSize: 18, fontWeight: FontWeight.normal),),
+                  style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.black, backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    side: BorderSide(color: Color(0xFFE8E8E8), width: 1.0),
+                  ),
+                ),
+                  )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
