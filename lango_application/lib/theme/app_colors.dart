fb5b6dfba6c6448c136e897269f16b7aa3947d7f
Chanakarn Limprasertsiri <minklim47@gmail.com>
2024-04-17 15:12:57 +0700

    [UPDATE] fix color code

--- lango_application/lib/theme/app_colors.dart
+++ lango_application/lib/theme/app_colors.dart
@@ -3,14 +3,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const red = Color(0xFF865E);
-  static const blue = Color(0xA2D2FF);
-  static const yellow = Color(0xFEE440);
-  static const cream = Color(0xFEF9EF);
+  static const red = Color(0xFFFF865E);
+  static const blue = Color(0xFFA2D2FF);
+  static const yellow = Color(0xFFFEE440);
+  static const cream = Color(0xFFFEF9EF);
 
-  static const darKGreen = Color(0x00983D);
-  static const green = Color(0x61D84D);
-  static const darkGrey = Color(0x5F5F5F);
-  static const black = Color(0x000000);
-  static const white = Color(0xFFFFFF);
+  static const darkGreen = Color(0xFF00983D);
+  static const green = Color(0xFF61D84D);
+  static const darkGrey = Color(0xFF5F5F5F);
+  static const black = Color(0xFF000000);
+  static const white = Color(0xFFFFFFFF);
 }
