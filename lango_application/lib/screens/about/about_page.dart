d78a3bb45b6e6e9a09b12cf2da51191a80ef8bf5
Chayapol Mahatthanachai <best37@hotmail.com>
2024-05-02 13:38:24 +0700

    Merge

--- lango_application/lib/screens/about/about_page.dart
+++ lango_application/lib/screens/about/about_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lango_application/widgets/navigator.dart';
@@ -9,7 +10,7 @@ class AboutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: AppColors.cream,
       bottomNavigationBar: BottomNav(path: "/about"),
       body: Wrapper(
@@ -20,7 +21,6 @@ class AboutPage extends StatelessWidget {
               child: Row(children: [
                 Text("About Us"),
               ])),
-          
           Expanded(
             child: Padding(
               padding: const EdgeInsets.only(bottom: 20.0),
@@ -29,15 +29,47 @@ class AboutPage extends StatelessWidget {
                 children: [
                   Text(
                       "Lango is a language learning app that helps users learn new languages through games and quizzes. Our app is designed to be user-friendly and interactive, making it easy for users to learn new languages in a fun and engaging way. With Lango, you can learn new languages at your own pace and on your own time. Whether you're a beginner or an advanced learner, Lango has something for everyone. Download our app today and start learning a new language with Lango!"),
-                  SizedBox(height: 40),
+                  SizedBox(height: 30),
+                  Divider(
+                    color: AppColors.grey,
+                    thickness: 2,
+                  ),
                   Padding(
-                      padding: const EdgeInsets.fromLTRB(20.0, 0, 10.0, 20),
-                      child: Text("Support us")),
-                  // Image.asset(
-                  //   'assets/QRcode.png',
-                  //   width: 200,
-                  //   height: 200,
-                  // ),
+                    padding: const EdgeInsets.fromLTRB(0, 10, 10.0, 10),
+                    child: ExpansionTile(
+                      title: Container(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text("Support us")),
+                      backgroundColor: AppColors.cream,
+                      collapsedBackgroundColor: AppColors.cream,
+                      children: [
+                        ListTile(
+                          title: Image.asset(
+                            'assets/images/QRcode.png',
+                            width: 200,
+                            height: 200,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Divider(
+                    color: AppColors.grey,
+                    thickness: 2,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 10, 10.0, 10),
+                    child: ExpansionTile(
+                      title: Container(
+                          padding: EdgeInsets.only(left: 20),
+                          child: Text("Survey")),
+                      backgroundColor: AppColors.cream,
+                      collapsedBackgroundColor: AppColors.cream,
+                      children: [
+                        ListTile(title: Text("insert survey here")),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
