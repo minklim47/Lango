5d1a47814bed3c40dbab094f15cf31a243ea492d
Watsawadee <109060632+Watsawadee@users.noreply.github.com>
2024-05-14 22:49:40 +0700

    [UPDATE] Upload img

--- lango_application/lib/screens/profile/edit_page.dart
+++ lango_application/lib/screens/profile/edit_page.dart
@@ -1,3 +1,4 @@
+import 'dart:typed_data';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +9,12 @@ import 'package:lango_application/utils/showSnackbar.dart';
 import 'package:lango_application/widgets/navigator.dart';
 import "package:lango_application/widgets/wrapper.dart";
 import 'package:lango_application/theme/custom_theme.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'dart:html' as html;
+
 import 'package:provider/provider.dart';
 
 class EditProfilePage extends StatefulWidget {
@@ -24,12 +31,14 @@ class _EditProfilePageState extends State<EditProfilePage> {
   late User? _currentUser;
   String _email = '';
   String _username = '';
+  String _profileImageUrl = '';
+  File? _image;
+  Uint8List? _webImage;
   CollectionReference users = FirebaseFirestore.instance.collection('users');
 
   @override
   void dispose() {
     editUsernameController.dispose();
-
     super.dispose();
   }
 
@@ -40,13 +49,64 @@ class _EditProfilePageState extends State<EditProfilePage> {
             .collection('users')
             .doc(FirebaseAuth.instance.currentUser!.uid)
             .update({'username': editUsernameController.text.trim()});
+
+        showSnackBar(context, "Username Updated");
         Provider.of<AppProvider>(context, listen: false)
             .updateUsername(editUsernameController.text.trim());
-        showSnackBar(context, "Username Updated");
         context.go('/profile');
       } catch (e) {
         showSnackBar(context, 'Error updating username : $e');
       }
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      if (kIsWeb) {
+        _webImage = await pickedFile.readAsBytes();
+      } else {
+        _image = File(pickedFile.path);
+      }
+    }
+    await _uploadImage();
+  }
+
+  Future<void> _uploadImage() async {
+    if (kIsWeb && _webImage == null) return;
+    if (!kIsWeb && _image == null) return;
+    try {
+      final storageRef = FirebaseStorage.instance
+          .ref()
+          .child('profile_images')
+          .child('${FirebaseAuth.instance.currentUser!.uid}.png');
+
+      UploadTask uploadTask;
+      if (kIsWeb) {
+        uploadTask = storageRef.putData(_webImage!);
+      } else {
+        uploadTask = storageRef.putFile(_image!);
+      }
+      // await uploadTask;
+
+      final downloadUrl = await storageRef.getDownloadURL();
+      setState(() {
+        _profileImageUrl = downloadUrl;
+      });
+
+      Provider.of<AppProvider>(context, listen: false)
+          .updateProfilePath(_profileImageUrl);
+
+      await users.doc(FirebaseAuth.instance.currentUser!.uid).update({
+        'profileImageUrl': _profileImageUrl,
+      });
+
+      showSnackBar(context, "Profile Picture Updated");
+    } catch (e) {
+      showSnackBar(context, 'Error uploading profile picture: $e');
+      print('Error uploading profile picture: $e');
     }
   }
 
@@ -69,7 +129,6 @@ class _EditProfilePageState extends State<EditProfilePage> {
         _username = userData['username'];
         _email = userData['email'];
       });
-      editUsernameController.text = _username;
     } else {
       setState(() {
         _currentUser = null;
@@ -95,12 +154,17 @@ class _EditProfilePageState extends State<EditProfilePage> {
             child: Stack(
               alignment: const Alignment(2, 1.2),
               children: [
-                const CircleAvatar(
+                CircleAvatar(
                   radius: 40,
-                  // backgroundImage: AssetImage('assets/avatar.png'),
+                  backgroundImage: _profileImageUrl.isNotEmpty
+                      ? NetworkImage(_profileImageUrl)
+                      : null,
+                  child: _profileImageUrl.isEmpty
+                      ? const Icon(Icons.person, size: 40)
+                      : null,
                 ),
                 IconButton(
-                  onPressed: () {},
+                  onPressed: _pickImage,
                   icon: const Icon(Icons.edit),
                   iconSize: 30,
                 ),
